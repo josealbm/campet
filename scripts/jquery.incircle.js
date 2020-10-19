090b176b37a0117701a6b71ccb6ed4409228c9aa
José Alberto <jmartinm@cifpfbmoll.eu>
2020-10-19 10:29:52 +0200

    small changes

---
+++ scripts/jquery.incircle.js
@@ -0,0 +1,49 @@
+(function($) {
+
+    $.fn.incircle = function(options) {
+
+        // Default options.
+        var settings = $.extend({
+            color: "#556b2f",
+            backgroundColor: "white",
+            type: 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
+            radius: '20em', //distance from center
+            start: -90, //shift start from 0
+            top: '350px',
+            left: '500px'
+        }, options);
+
+        this.css({
+            'position': 'relative',
+            'top': settings.top,
+            'left': settings.left,
+            'list-style-type': 'none',
+            'margin': 'auto',
+            'padding': 'auto'
+        });
+
+        $elements = this.children(':not(:first-child)');
+        numberOfElements = (settings.type === 1) ? $elements.length : $elements.length - 1; //adj for even distro of elements when not full circle
+        slice = 360 * settings.type / numberOfElements;
+
+        $elements.each(function(i) {
+            var $self = $(this),
+                rotate = slice * i + settings.start,
+                rotateReverse = rotate * -1;
+
+            $self.css({
+                'position': 'absolute',
+                '-webkit-transition': 'all 2s linear',
+                '-moz-transition': 'all 2s linear',
+                'transition': 'all 2s linear'
+            });
+
+            $self.css({
+                'transform': 'rotate(' + rotate + 'deg) translate(' + settings.radius + ') rotate(' + rotateReverse + 'deg)'
+            });
+        });
+
+        return this;
+    };
+
+}(jQuery));
---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('#circlemenu').incircle();
+});
+$('#circlemenu').incircle({
+    type: 1
+});
+$('#circlemenu').incircle({
+    radius: '20em'
+});
+// $('#circlemenu').incircle({
+//     radius: '10em'
+// });
+$('#circlemenu').incircle({
+    top: '500px',
+    left: '500px'
+});
+$('#circlemenu').incircle({
+    start: -90
+});
