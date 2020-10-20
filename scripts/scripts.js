86e57da8a34746b91f1c69a194a207bf55732f37
José Alberto <mem3nto@gmail.com>
2020-10-21 00:29:46 +0200

    fixed favicon and navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,5 @@
+window.onload = function(){
 $(function() {
     $('#circlemenu').incircle();
-});
-// $('#circlemenu').incircle({
-//     type: 1
-// });
-// $('#circlemenu').incircle({
-//     radius: '20em'
-// });
-// // $('#circlemenu').incircle({
-// //     radius: '10em'
-// // });
-// $('#circlemenu').incircle({
-//     top: '500px',
-//     left: '500px'
-// });
-// $('#circlemenu').incircle({
-//     start: -90
-// });
+})
+};
