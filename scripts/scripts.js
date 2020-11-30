84ddc20831b9fe481fdd900ee57f35f8f4cff458
José Alberto <mem3nto@gmail.com>
2020-11-30 13:02:36 +0100

    Added tooltips on menu page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // $('body').css('background', 'url("/images/LOGOS-DIFERENTS-MIDES/PER-WEB-XARXES-SOCIALS/1600X1200px/positiu-negre-verd.png") no-repeat')
     $(function () {
         $("#header").load("header.html");
         $("#footer").load("footer.html");
     });
 
-    $(function() { 
-        var elem = $('.circle').hide(); 
-               var i = 0; 
-               (function displayImages() { 
-                      elem.eq(i++).fadeIn(500, displayImages).css("display", "inline-block"); 
-               })();   
-     });   
+    $(function () {
+        var elem = $('.circle').hide();
+        var i = 0;
+        (function displayImages() {
+            elem.eq(i++).fadeIn(500, displayImages).css("display", "inline-block");
+        })();
+    });
+    $(function () {
+        $(document).tooltip();
+    });
 });
 
 
