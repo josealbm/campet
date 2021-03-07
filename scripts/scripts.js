75dd8e9eadb36eaac055757f7e84f3494c5d7f99
Berto Linux <mem3nto@gmail.com>
2021-03-07 19:26:14 +0100

    cambios en lenguajes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     // $('body').css('background', 'url("/images/LOGOS-DIFERENTS-MIDES/PER-WEB-XARXES-SOCIALS/1600X1200px/positiu-negre-verd.png") no-repeat')
     $(function () {
-        $("#header").load("header.html");
-        $("#footer").load("footer.html");
+        $("#header").load("./partials/header.html");
+        $("#footer").load("./partials/footer.html");
     });
 
     $(function () {
@@ -13,26 +13,39 @@ $(document).ready(function () {
         })();
     });
     $(function () {
-        $(document).tooltip();
+        $('.circle').tooltip();
     });
 
-    // $(function(){
-    //     $('.selectpicker').selectpicker();
-    //     console.log("patata");
-    // });
+       
+//     $(function() {
+//         $('#switch-lang').bootstrapToggle({
+//           on: 'Català',
+//           off: 'Castellano'
+//         });
+//       })
+   
 });
 
 
-var sourceSwap = function () {
-    var $this = $(this);
-    var newSource = $this.data('alt-src');
-    $this.data('alt-src', $this.attr('src'));
-    $this.attr('src', newSource);
+// var sourceSwap = function () {
+//     var $this = $(this);
+//     var newSource = $this.data('alt-src');
+//     $this.data('alt-src', $this.attr('src'));
+//     $this.attr('src', newSource);
+// }
+
+// $(function () {
+//     $('img[data-alt-src]').each(function () {
+//         new Image().src = $(this).data('alt-src');
+//     }).hover(sourceSwap, sourceSwap);
+// });
+$('.switch-lang').change(function(){
+if ($(this).prop('checked')){
+    $(".cat").show()
+    $(".es").hide()
+    console.log('patata')
+}else{
+    $(".es").show()
+    $(".cat").hide()
 }
-
-$(function () {
-    $('img[data-alt-src]').each(function () {
-        new Image().src = $(this).data('alt-src');
-    }).hover(sourceSwap, sourceSwap);
-});
-
+})
